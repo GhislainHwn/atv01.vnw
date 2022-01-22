231f424913276985a767d136821ef007024fb91f
GhislainHwn <clovisleron@gmail.com>
2022-01-22 10:53:33 -0300

    update project

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Ghislain";
+console.log("Ghislain é seu nome");
+let nIdade= 26;
+console.log(nIdade);
+const anoAtualSubtraido= 2022-27;
+console.log(anoAtualSubtraido);
+const somaDuas= 27+1995;
+console.log(somaDuas)
